821f2cb6bfa5f4eea4b97a930dc08edecd2840db
mckellar854 <71614690+mckellar854@users.noreply.github.com>
2020-10-25 23:11:36 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,77 @@
+// Analyzes the text to discover if there are any vowels, consonants, digits, spaces, and unknown characters.
+// CodeHunter.cpp : Defines the entry point for the console application.
+// This line only needed for console C++ project, omit this for Empty C++ Project.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	string textToAnalyze;
+	int foo = 0;
+	int vowels = 0;
+	int consonants = 0;
+	int digits = 0;
+	int spaces = 0;
+	int lengthOfStringSubmittedForAnalysis = 0;
+	int unknownCharacters = 0;
+	int checkSum = 0;
+	int bar = 0;
+
+	cout << "Welcome to the CIA code Hunter Program!" << endl;
+	cout << "Please type in text to analyze: " << endl;
+	getline(cin, textToAnalyze);
+
+	for (int i = 0; i < textToAnalyze.length(); ++i)
+	{
+		if (textToAnalyze[i] == 'a' || textToAnalyze[i] == 'e' || textToAnalyze[i] == 'i' ||
+			textToAnalyze[i] == 'o' || textToAnalyze[i] == 'u' || textToAnalyze[i] == 'A' ||
+			textToAnalyze[i] == 'E' || textToAnalyze[i] == 'I' || textToAnalyze[i] == 'O' ||
+			textToAnalyze[i] == 'U')
+		{
+			++vowels;
+		}
+		else if ((textToAnalyze[i] >= 'a' && textToAnalyze[i] <= 'z') || (textToAnalyze[i] >= 'A' && textToAnalyze[i] <= 'Z'))
+		{
+			++consonants;//++consonants;
+		}
+		else if (textToAnalyze[i] >= '0' && textToAnalyze[i] <= '9')
+		{
+			++digits;
+		}
+		else if (textToAnalyze[i] == ' ')
+		{
+			++spaces;
+		}
+		else
+		{
+			++unknownCharacters;
+		}
+	}
+
+	lengthOfStringSubmittedForAnalysis = textToAnalyze.length();
+	checkSum = unknownCharacters + vowels + consonants + digits + spaces;
+
+	cout << "vowels: " << vowels << endl;
+	cout << "consonants: " << consonants << endl;
+	cout << "digits: " << digits << endl;
+	cout << "spaces: " << spaces << endl;
+	cout << "Length of string submitted for analysis: " << lengthOfStringSubmittedForAnalysis << endl;
+	cout << "Number of characters CodeHunter could not identify: " << unknownCharacters << endl;
+	cout << "Checksum: " << checkSum << endl;
+
+	if (checkSum == lengthOfStringSubmittedForAnalysis)
+	{
+		cout << "This program self checking has found this Analysis to be valid." << endl;
+	}
+	else
+	{
+		cout << "WARNING! *** This program self checking has found this Analysis to be invalid! Do not use this data!" << endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
